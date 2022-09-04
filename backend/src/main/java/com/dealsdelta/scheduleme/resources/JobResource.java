da3a1a0cc9d8a6a43805adb560d491c134276c4b
--get <kumargauravgupta3@gmail.com>
2022-09-04 14:31:31 +0530

    Refactors JobResource and adds corresponding Unit Tests

--- backend/src/main/java/com/dealsdelta/scheduleme/resources/JobResource.java
+++ backend/src/main/java/com/dealsdelta/scheduleme/resources/JobResource.java
@@ -2,6 +2,7 @@ package com.dealsdelta.scheduleme.resources;
 
 
 import com.dealsdelta.scheduleme.dtos.*;
+import com.dealsdelta.scheduleme.services.JobService;
 import com.dealsdelta.scheduleme.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,213 +21,64 @@ import java.util.Map;
 public class JobResource {
 
     @Autowired
-    private TaskService service;
-
-    @GetMapping
-    public List<IJob> getJobs(@RequestParam("status") String status) {
-        return service.getJobs(status);
-    }
-
-    @GetMapping("/jobs/{jobId}")
-    public IJob getJob(@PathVariable("jobId") String jobId) {
-        return service.getJob(jobId);
-    }
-
-    @PostMapping("/daily-jobs")
-    public DailyJob createDailyJob(@RequestBody DailyJob job) {
-        return service.createDailyJob(job);
-    }
+    private JobService service;
 
     @GetMapping("/daily-jobs")
-    public List<DailyJob> getDailyJobs() {
-        return service.getDailyJobs();
-    }
-
-    @PutMapping("/daily-jobs/{jobId}")
-    public DailyJob runDailyJob(@PathVariable("jobId") String jobId) {
-        return service.runDailyJob(jobId);
-    }
-
-    @DeleteMapping("/daily-jobs/{jobId}")
-    public void deleteDailyJobs(@PathVariable("jobId") String jobId) {
-        service.deleteDailyJob(jobId);
-    }
-
-    @PostMapping("/hourly-jobs")
-    public HourlyJob createHourly(@RequestBody HourlyJob job) {
-        return service.createHourlyJob(job);
-    }
-
-    @GetMapping("/hourly-jobs")
-    public List<HourlyJob> getHourlyJobs() {
-        return service.getHourlyJobs();
-    }
-
-    @DeleteMapping("/hourly-jobs/{jobId}")
-    public void deleteHourlyJobs(@PathVariable("jobId") String jobId) {
-        service.deleteHourlyJob(jobId);
-    }
-
-    @PutMapping("/hourly-jobs/{jobId}")
-    public HourlyJob runHourlyJob(@PathVariable("jobId") String jobId) {
-        return service.runHourlyJob(jobId);
-    }
-
-    @PostMapping("/monthly-jobs")
-    public MonthlyJob createMonthly(@RequestBody MonthlyJob job) {
-        return service.createMonthlyJob(job);
+    public List<Job> getDailyJobs() {
+       return service.getDailyJobs();
     }
 
     @GetMapping("/monthly-jobs")
-    public List<MonthlyJob> getMonthlyJobs() {
+    public List<Job> getMonthlyJobs() {
         return service.getMonthlyJobs();
     }
 
-    @DeleteMapping("/monthly-jobs/{jobId}")
-    public void deleteMonthlyJobs(@PathVariable("jobId") String jobId) {
-        service.deleteMonthlyJob(jobId);
+    @GetMapping("/nmonthly-jobs")
+    public List<Job> getNMonthlyJobs() {
+        return service.getNMonthlyJobs();
     }
 
-    @PutMapping("/monthly-jobs/{jobId}")
-    public MonthlyJob runMonthlyJob(@PathVariable("jobId") String jobId) {
-        return service.runMonthlyJob(jobId);
+    @GetMapping("/weekly-jobs")
+    public List<Job> getWeeklyJobs() {
+        return service.getWeeklyJobs();
     }
 
-    @PostMapping("/record-processor-jobs")
-    public RecordProcessorJob createRecordProcessorJob(@RequestBody RecordProcessorJob job) {
-        return service.createRecordProcessorJob(job);
+    @GetMapping("/nweekly-jobs")
+    public List<Job> getNWeeklyJobs() {
+        return service.getNWeeklyJobs();
     }
 
-    @GetMapping("/record-processor-jobs")
-    public List<RecordProcessorJob> getRecordProcessorJobs() {
-        return service.getRecordProcessorJobs();
+    @GetMapping("/run-once-jobs")
+    public List<Job> getRunOnceJobs() {
+        return service.getRunOnceJobs();
     }
 
-    @DeleteMapping("/record-processor-jobs/{jobId}")
-    public void deleteRecordProcessorJobs(@PathVariable("jobId") String jobId) {
-        service.deleteRecordProcessorJob(jobId);
-    }
-
-    @PutMapping("/record-processor-jobs/{jobId}")
-    public RecordProcessorJob runRecordProcessorJob(@PathVariable("jobId") String jobId) {
-        return service.runRecordProcessorJob(jobId);
-    }
-
-    @PostMapping("/generic-jobs")
-    public GenericJob createDailyJob(@RequestBody GenericJob job) {
-        return service.createGenericJob(job);
-    }
-
-    @GetMapping("/generic-jobs")
-    public List<GenericJob> getGenericJobs() {
-        return service.getGenericJobs();
-    }
-
-    @DeleteMapping("/generic-jobs/{jobId}")
-    public void deleteGenericJobs(@PathVariable("jobId") String jobId) {
-        service.deleteGenericJob(jobId);
-    }
-
-    @PutMapping("/generic-jobs/{jobId}")
-    public GenericJob runGenericJob(@PathVariable("jobId") String jobId) {
-        return service.runGenericJob(jobId);
-    }
-
-
-    @PostMapping("/record-processor-daily-jobs")
-    public RecordProcessorDailyJob createRecordProcessorDailyJobs(@RequestBody RecordProcessorDailyJob job) {
-        return service.createRecordProcessorHourlyJob(job);
-    }
-
-    @GetMapping("/record-processor-daily-jobs")
-    public List<RecordProcessorDailyJob> getRecordProcessorDailyJobs() {
-        return service.getRecordProcessorDailyJobs();
-    }
-
-    @DeleteMapping("/record-processor-daily-jobs/{jobId}")
-    public void deleteRecordProcessorDailyJobs(@PathVariable("jobId") String jobId) {
-        service.deleteRecordProcessorDailyJob(jobId);
-    }
-
-    @PutMapping("/record-processor-daily-jobs/{jobId}")
-    public RecordProcessorDailyJob runRecordProcessorDailyJob(@PathVariable("jobId") String jobId) {
-        return service.runRecordProcessorDailyJob(jobId);
-    }
-
-    @PostMapping("/record-processor-monthly-jobs")
-    public RecordProcessorMonthlyJob createRecordProcessorMonthlyJob(@RequestBody RecordProcessorMonthlyJob job) {
-        return service.createRecordProcessorHourlyJob(job);
-    }
-
-    @GetMapping("/record-processor-monthly-jobs")
-    public List<RecordProcessorMonthlyJob> getRecordProcessorMonthlyJobs() {
-        return service.getRecordProcessorMonthlyJobs();
-    }
-
-    @DeleteMapping("/record-processor-monthly-jobs/{jobId}")
-    public void deleteRecordProcessorMonthlyJobs(@PathVariable("jobId") String jobId) {
-        service.deleteRecordProcessorMonthlyJob(jobId);
-    }
-
-    @PutMapping("/record-processor-monthly-jobs/{jobId}")
-    public RecordProcessorMonthlyJob runRecordProcessorMonthlyJob(@PathVariable("jobId") String jobId) {
-        return service.runRecordProcessorMonthlyJob(jobId);
-    }
-
-    @PostMapping("/record-processor-hourly-jobs")
-    public RecordProcessorHourlyJob createRecordProcessorHourlyJob(@RequestBody RecordProcessorHourlyJob job) {
-        return service.createRecordProcessorHourlyJob(job);
-    }
-
-    @GetMapping("/record-processor-hourly-jobs")
-    public List<RecordProcessorHourlyJob> getRecordProcessorHourlyJobs() {
-        return service.getRecordProcessorHourlyJobs();
-    }
-
-    @DeleteMapping("/record-processor-hourly-jobs/{jobId}")
-    public void deleteRecordProcessorHourlyJobs(@PathVariable("jobId") String jobId) {
-        service.deleteRecordProcessorHourlyJob(jobId);
-    }
-
-    @PutMapping("/record-processor-hourly-jobs/{jobId}")
-    public RecordProcessorHourlyJob runRecordProcessorHourlyJob(@PathVariable("jobId") String jobId) {
-        return service.runRecordProcessorHourlyJob(jobId);
-    }
-
-    @GetMapping("/jobs/{jobId}/logs")
-    public List<Log> getJobLogs(@PathVariable String jobId, @RequestParam(name = "page", defaultValue = "0") int page,
-                                     @RequestParam(name = "count", defaultValue = "1000") int count) {
-        return service.getLogs(jobId, page, count);
-    }
-
-    @GetMapping("/jobs/{jobId}/audit")
-    public List<JobAudit> getJopAudits(@PathVariable String jobId, @RequestParam(name = "page", defaultValue = "0") int page,
-                                @RequestParam(name = "count", defaultValue = "1000") int count) {
-        return service.getJobAudits(jobId, page, count);
+    @GetMapping("/last-day-of-month-jobs")
+    public List<Job> getLastDayOfMonthJobs() {
+        return service.getLastDayOfMonthJobs();
     }
 
     @GetMapping("/jobs-count")
-    public Map<String, Long> getAllJobsCount() {
-        return service.getAllJobsCount();
+    public long getJobCount() {
+        return service.getAllJobCount();
     }
 
-    @GetMapping("/running-jobs-count")
-    public long getRunningJobCount() {
-        return service.getRunningJobCount();
+    @DeleteMapping("/jobs/{jobId}")
+    public void removeJob(@PathVariable("jobId") String jobId) {
+        service.removeJob(jobId);
     }
 
-    @GetMapping("/job-run-per-day")
-    public List<JobRunStat> getJobsRanByDay() {
-        return service.getJobsRanByDay();
+    @GetMapping("/jobs/{jobId}")
+    public Job getJob(@PathVariable("jobId") String jobId) {
+       return service.getJob(jobId);
     }
 
-    @GetMapping("/job-failed-per-day")
-    public List<JobRunStat> getJobFailedByDay() {
-        return service.getJobFailedByDay();
+    @PutMapping("/jobs")
+    public Job updateJob(@RequestBody Job job) {
+        return service.updateJob(job);
     }
 
-    @GetMapping("/create-bogus-data")
-    public List<JobAudit> createBogusAudit() {
-        return service.createBogusAudit();
-    }
+
+
+
 }
---
+++ backend/src/test/java/com/dealsdelta/scheduleme/resources/JobResourceTest.java
@@ -0,0 +1,187 @@
+package com.dealsdelta.scheduleme.resources;
+
+import com.dealsdelta.scheduleme.dtos.Job;
+import com.dealsdelta.scheduleme.dtos.RUN_FREQUENCY;
+import com.dealsdelta.scheduleme.services.JobService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.reflect.Whitebox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+/**
+ * Description : <Write class Description>
+ * Author: kumar
+ * Created on : 04/09/22
+ */
+
+class JobResourceTest {
+    private JobResource resource;
+    private JobService service;
+    @BeforeEach
+    void setUp() {
+        resource = new JobResource();
+        service = PowerMockito.mock(JobService.class);
+        Whitebox.setInternalState(resource, "service", service);
+    }
+
+    @Test
+    void getDailyJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.DAILY);
+            jobs.add(job);
+        }
+        when(service.getDailyJobs()).thenReturn(jobs);
+        List<Job> results = resource.getDailyJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.DAILY, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getMonthlyJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.MONTHLY);
+            jobs.add(job);
+        }
+        when(service.getMonthlyJobs()).thenReturn(jobs);
+        List<Job> results = resource.getMonthlyJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.MONTHLY, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getNMonthlyJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.N_MONTHLY);
+            jobs.add(job);
+        }
+        when(service.getNMonthlyJobs()).thenReturn(jobs);
+        List<Job> results = resource.getNMonthlyJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.N_MONTHLY, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getWeeklyJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.WEEKLY);
+            jobs.add(job);
+        }
+        when(service.getWeeklyJobs()).thenReturn(jobs);
+        List<Job> results = resource.getWeeklyJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.WEEKLY, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getNWeeklyJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.N_WEEKLY);
+            jobs.add(job);
+        }
+        when(service.getNWeeklyJobs()).thenReturn(jobs);
+        List<Job> results = resource.getNWeeklyJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.N_WEEKLY, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getRunOnceJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.ONCE);
+            jobs.add(job);
+        }
+        when(service.getRunOnceJobs()).thenReturn(jobs);
+        List<Job> results = resource.getRunOnceJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.ONCE, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getLastDayOfMonthJobs() {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Job job = new Job();
+            job.setFrequency(RUN_FREQUENCY.LAST_DAY_OF_MONTH);
+            jobs.add(job);
+        }
+        when(service.getLastDayOfMonthJobs()).thenReturn(jobs);
+        List<Job> results = resource.getLastDayOfMonthJobs();
+        for(Job j : results) {
+            assertEquals(RUN_FREQUENCY.LAST_DAY_OF_MONTH, j.getFrequency());
+        }
+    }
+
+    @Test
+    void getJobCount() {
+        long count = 100;
+        when(service.getAllJobCount()).thenReturn(count);
+        long result = resource.getJobCount();
+        assertEquals(count, result);
+    }
+
+    @Test
+    void removeJob() {
+        String jobId = "jobId";
+        doNothing().when(service).removeJob(jobId);
+        resource.removeJob(jobId);
+    }
+
+    @Test
+    void getJob() {
+        Job job = new Job();
+        String jobId = "jobId";
+        job.setJobId(jobId);
+        when(service.getJob(jobId)).thenReturn(job);
+        Job result = resource.getJob(jobId);
+        assertEquals(jobId, result.getJobId());
+    }
+
+    @Test
+    void updateJob() {
+        Job job = new Job();
+        String jobId = "jobId";
+        job.setJobId(jobId);
+        String updatedName = "UpdatedName";
+        when(service.updateJob(job)).thenAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Object argument = invocationOnMock.getArgument(0);
+                Job j = (Job) argument;
+                j.setJobName(updatedName);
+                return j;
+            }
+        });
+        Job result = resource.updateJob(job);
+        assertEquals(updatedName, result.getJobName());
+    }
+
+
+}
