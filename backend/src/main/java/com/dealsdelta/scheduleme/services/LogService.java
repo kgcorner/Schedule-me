cf2a172d0d6c4fbc6675f5123449b8be5c808d1c
--get <kumargauravgupta3@gmail.com>
2022-09-06 16:46:36 +0530

    Adds a method to create log for Job model

--- backend/src/main/java/com/dealsdelta/scheduleme/services/LogService.java
+++ backend/src/main/java/com/dealsdelta/scheduleme/services/LogService.java
@@ -4,7 +4,9 @@ package com.dealsdelta.scheduleme.services;
 import com.dealsdelta.scheduleme.data.dao.LogDao;
 import com.dealsdelta.scheduleme.data.models.LogModel;
 import com.dealsdelta.scheduleme.dtos.IJob;
+import com.dealsdelta.scheduleme.dtos.Job;
 import com.dealsdelta.scheduleme.dtos.Log;
+import com.dealsdelta.scheduleme.util.ExcludeFromJacocoGeneratedReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +23,11 @@ public class LogService {
     @Autowired
     private LogDao logDao;
 
+    @ExcludeFromJacocoGeneratedReport
     public void write(IJob job, String level, String message, String module, String runId) {
+    }
+
+    public void write(Job job, String level, String message, String module, String runId) {
         LogModel log = new LogModel();
         log.setDate(new Date());
         log.setJobId(job.getJobId());
@@ -30,5 +36,9 @@ public class LogService {
         log.setLevel(level);
         log.setRunId(runId);
         logDao.create(log);
+    }
+
+    public void write(Job job, String level, String message, Class module, String runId) {
+        write(job, level, message, module.getName(), runId);
     }
 }
